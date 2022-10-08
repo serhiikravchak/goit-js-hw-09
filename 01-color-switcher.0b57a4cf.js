4c1afcb194abe6fe11ca8b56803f883618b87618
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2022-10-08 15:28:09 +0000

    Deploying to gh-pages from @ serhiikravchak/goit-js-hw-09@503a8af2efddd41f068f6b79ee6957f239d87767 🚀

---
+++ 01-color-switcher.0b57a4cf.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){t.disabled=!0,n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.0b57a4cf.js.map
---
+++ 01-color-switcher.2f1e7294.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{t.disabled=!0,n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.2f1e7294.js.map
